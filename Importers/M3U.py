4505eb624db2e674bf8535e7ad1077fa8b20f9e1
Matt Spitz <mattspitz@gmail.com>
2015-12-04 09:31:56 -0500

    Removing odd 'absolute path' logic. Linux FTW.

--- Importers/M3U.py
+++ Importers/M3U.py
@@ -1,17 +1,14 @@
 import Importers
 import os
-import re
 
 def _ImportMe(PlaylistPath):
     PlaylistFiles=[]
-    PlaylistDirPath=os.path.dirname(PlaylistPath)+os.sep
     try:
         with open(PlaylistPath, 'r', encoding='utf-8') as PlaylistFileHandle: #Specify UTF-8 here to avoid Unicode errors
             for LineStr in PlaylistFileHandle:
                 if(LineStr[0]=='#'): #Ignore comments/controls
                     continue
-                IsAbsolutePath=(LineStr[0:2]=='\\\\' or re.match('[a-z]:\\\\', LineStr, flags=re.I)) #Check for absolute path syntax (smb or drive letter)
-                LookupPath=os.path.realpath(('' if IsAbsolutePath else PlaylistDirPath)+LineStr.rstrip('\n'))
+                LookupPath = LineStr.strip()
                 if(not os.path.isfile(LookupPath)): #Confirm file exists
                     raise Exception('PlaylistFileNotFound', LineStr)
                 PlaylistFiles.append(LookupPath) #Add the relative path to the playlist file list
